26dd6f0ee5a7e5dbabbb02ee79ef6144e28238eb
Dennis Lieu <dlieu@lieudennis.com>
2015-11-01 20:08:19 +0100

    EnemySpawner

---
+++ Assets/Scripts/View/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.mediation.impl;
+using System;
+using System.Collections.Generic;
+
+public class EnemySpawner : EventView
+{
+    [Inject]
+    public SoundtrackSignal SoundtrackSignal { get; set; }
+
+    [SerializeField]
+    private List<GameObject> _enemySpawnList;
+
+    [SerializeField]
+    private List<float> _enemySpawnTime;
+
+    [SerializeField]
+    private List<Vector2> _enemySpawnVector;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+
+        SoundtrackSignal.AddListener(OnSoundtrackSignal);
+    }
+
+    private void OnSoundtrackSignal(float seconds)
+    {
+        for (int i = 0; i < _enemySpawnList.Count; i++)
+        {
+            float time = _enemySpawnTime[i];
+
+            if (time == seconds)
+            {
+                GameObject enemy = _enemySpawnList[i];
+                Vector2 positon = _enemySpawnVector[i];
+
+                Instantiate(enemy, new Vector3(positon.x, positon.y, 0), Quaternion.identity);
+            }
+        }
+    }
+}
